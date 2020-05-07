5b527b4c8503a88947beb6078bd292bdd1ee386f
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-07 20:02:10 +0530

    getCurrentBuildMetrics

--- vars/getCurrentBuildMetrics.groovy
+++ vars/getCurrentBuildMetrics.groovy
@@ -1,6 +1,6 @@
 def call(){
-sh "value='cat /var/lib/jenkins/workspace/${JOB_NAME}/buildSessionId'"
-bsid= bsId.getText('UTF-8')
+sh "bsid='cat /var/lib/jenkins/workspace/${JOB_NAME}/buildSessionId'"
+
 println(bsid)
 sh """curl --location --request GET 'https://wipro.sealights.co/sl-api/v1/coverage/builds/$bsid' \
 --header 'Authorization: Bearer $apiToken' """
