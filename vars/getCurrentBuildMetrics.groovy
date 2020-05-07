255e4c4f7c84cfdbd4e6fd0349f1d6105683bf11
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-07 19:47:06 +0530

    getCurrentBuildMetrics

--- vars/getCurrentBuildMetrics.groovy
+++ vars/getCurrentBuildMetrics.groovy
@@ -1,6 +1,6 @@
 def call(){
-filename = '/var/lib/jenkins/workspace/${JOB_NAME}'
-file bsId = newFile(filename)
+def filename = "/var/lib/jenkins/workspace/${JOB_NAME}/buildSessionId"
+sh "value=$(<${filename})"
 bsid= bsId.getText('UTF-8')
 println(bsid)
 sh """curl --location --request GET 'https://wipro.sealights.co/sl-api/v1/coverage/builds/$bsid' \
