5cdd856348afda24d797ea8e4e8f801c29390992
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-07 18:02:29 +0530

    sealights token groovy added

--- vars/sealights_token.groovy
+++ vars/sealights_token.groovy
@@ -1,7 +1,7 @@
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
-String a=jsonObj.sealights.token
+String a=jsonObj.sealights.agent_token
   println(a)
   sh "echo '$a' >> node_sltoken.txt"
 }
--- vars/setQualityGate.groovy
+++ vars/setQualityGate.groovy
@@ -2,5 +2,27 @@ def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 def mC=jsonObj.sealights.modifiedCoverage
-print(mC)
+def oC=jsonObj.sealights.overallCoverage
+def apiToken=jsonObj.sealights.api_token
+sh """ curl --location --request POST 'https://wipro.sealights.co//sl-api/v1/apps/digital_game/quality-gates' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer $apiToken' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "failedTests": {
+    "testStages": [ "stage" ],
+    "entireBuild": false
+  },
+  "modifiedCoverage": {
+    "value": $mC,
+    "testStages": [ "Combined Across Stages" ],
+    "entireBuild": false
+  },
+  "overallCoverage": {
+    "value": $oC,
+    "testStages": [ "Combined Across Stages" ],
+    "entireBuild": false
+  }
+}' """
+
 }
