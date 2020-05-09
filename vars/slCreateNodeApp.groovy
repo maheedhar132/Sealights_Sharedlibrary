d396d7fd4cc257157fd52eccf31e51ee5e8b6463
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-09 20:11:04 +0530

    sl groovy edited

--- vars/slCreateNodeApp.groovy
+++ vars/slCreateNodeApp.groovy
@@ -1,12 +1,21 @@
-def call(gitRepoName,tokenFileName,gitUrl){
-sh "git clone ${gitUrl}"
+def call(jsondata){
+
+
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+String a = ci.pipelines.pipeline.pipeline_name
+String job_name = a.replace("[","").replace("]","")
+
+
+
+sh "git clone https://github.com/maheedhar132/Game.git"
 sh "cd '${gitRepoName}' && sudo npm i slnodejs"
 sh "cd '${gitRepoName}'/client && sudo npm i slnodejs"
-sh "sh "./node_modules/.bin/slnodejs config --tokenfile /var/lib/jenkins/workspace/${JOB_NAME}/${tokenFileName} --appname '${JOB_NAME}' --branch 'master' --build 1""
-sh "cd '${gitRepoName}'/client && sudo npm install"
+sh "./node_modules/.bin/slnodejs config --tokenfile /var/lib/jenkins/workspace/${JOB_NAME}/node_sltoken.txt --appname '${job_name}' --branch 'master' --build 1"
+/*sh "cd '${gitRepoName}'/client && sudo npm install"
 sh "cd '${gitRepoName}' && sudo npm install"
 sh "cd '${gitRepoName}'/client && sudo npm run build"
 sh 'cd '${gitRepoName}'/client && ./node_modules/.bin/slnodejs build --tokenfile /var/lib/jenkins/workspace/${JOB_NAME}/node_sltoken.txt --buildsessionidfile /var/lib/jenkins/workspace/${JOB_NAME}/buildSessionId --instrumentForBrowsers  --workspacepath build --outputpath sl_build --scm none'
 sh "mv '${gitRepoName}'/client/build '${gitRepoName}'/client/old_build"
-sh "mv '${gitRepoName}'/client/sl_build '${gitRepoName}'/client/build"
+sh "mv '${gitRepoName}'/client/sl_build '${gitRepoName}'/client/build"*/
 }
