945f26854781b355f92642369f7bd8ce058b4ec6
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-09 20:30:27 +0530

    sl groovy edited

--- vars/slCreateNodeApp.groovy
+++ vars/slCreateNodeApp.groovy
@@ -12,11 +12,11 @@ sh "sudo rm -rf Game"
 sh "git clone https://github.com/maheedhar132/Game.git"
 sh "cd Game && sudo npm i slnodejs"
 sh "cd Game/client && sudo npm i slnodejs"
-sh "./node_modules/.bin/slnodejs config --tokenfile /var/lib/jenkins/workspace/${JOB_NAME}/node_sltoken.txt --appname '${job_name}' --branch 'master' --build 1"
+sh "./node_modules/.bin/slnodejs config --tokenfile /var/lib/jenkins/workspace/Game/node_sltoken.txt --appname 'Game' --branch 'master' --build 1"
 /*sh "cd Game/client && sudo npm install"
 sh "cd Game && sudo npm install"
 sh "cd Game/client && sudo npm run build"
-sh 'cd Game/client && ./node_modules/.bin/slnodejs build --tokenfile /var/lib/jenkins/workspace/${JOB_NAME}/node_sltoken.txt --buildsessionidfile /var/lib/jenkins/workspace/${JOB_NAME}/buildSessionId --instrumentForBrowsers  --workspacepath build --outputpath sl_build --scm none'
+sh 'cd Game/client && ./node_modules/.bin/slnodejs build --tokenfile /var/lib/jenkins/workspace/Game/node_sltoken.txt --buildsessionidfile /var/lib/jenkins/workspace/Game/buildSessionId --instrumentForBrowsers  --workspacepath build --outputpath sl_build --scm none'
 sh "mv Game/client/build Game/client/old_build"
 sh "mv Game/client/sl_build Game/client/build"*/
 }
