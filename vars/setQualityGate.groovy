350c9f62f3f413b10493029d9edf8006e24610fe
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-11 22:58:33 +0530

    Required edits

--- vars/setQualityGate.groovy
+++ vars/setQualityGate.groovy
@@ -1,8 +1,9 @@
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
-def mC=jsonObj.sealights.modifiedCoverage
-def oC=jsonObj.sealights.overallCoverage
+def mC=jsonObj.code_quality.applications.application[0].quality_gate[0].metrics[0].error
+println(mC)
+def oC=jsonObj.code_quality.applications.application[0].quality_gate[0].metrics[1].error
 def apiToken=jsonObj.sealights.api_token
 sh """ curl --location --request POST 'https://wipro.sealights.co//sl-api/v1/apps/${JOB_NAME}/quality-gates' \
 --header 'Content-Type: application/json' \
