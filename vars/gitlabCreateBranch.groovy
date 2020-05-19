a9332b21b50439a8dc3b85a6f4c4211cae7dc7d7
maheerdhar132 <maheedhartalluri@gmail.com>
2020-05-19 18:49:01 +0530

    Git validator added

--- vars/gitlabCreateBranch.groovy
+++ vars/gitlabCreateBranch.groovy
@@ -3,7 +3,8 @@ import groovy.json.*
 @NonCPS
 fetchBranchlist(gitlab_url,gitlab_user,projName,api_token)
 {
-	String url=gitlab_url+'/api/v4/projects/'+gitlab_user+'%2F'+projName+'/repository/branches?private_token='+api_token
+	String	projCopy=projName.replace("\n","")
+	String url=gitlab_url+'/api/v4/projects/'+gitlab_user+'%2F'+projCopy+'/repository/branches?private_token='+api_token
     println(url)
 	sh """
  curl --location --request GET '${url}' \
