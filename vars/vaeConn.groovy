ca8fa392da17ec7e6c2de7d77b22ca21e8230664
maheerdhar132 <maheedhartalluri@gmail.com>
2020-06-03 18:59:37 +0530

    vae added

--- vars/vaeConn.groovy
+++ vars/vaeConn.groovy
@@ -178,9 +178,11 @@ triggerOrch(accessToken,depID)
 String runIDvar = new File("/var/lib/jenkins/workspace/${JOB_NAME}/triggerOrch.json").text
 println(runIDvar)
 println(runIDvar.length())
+if(runIDvar.length()!=67){
 String runIDjson = runIDvar.substring(runIDvar.indexOf("[") + 1, runIDvar.indexOf("]"))
-
-
+}
+def runIDjson = readJSON text: runIDjson
+String runID = runIDjsonvar.runid
 
 
 fetchOrchestrationStatus(accessToken,depID,runID)
